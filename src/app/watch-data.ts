afcf5e095dab74471ca54cecffb34c1c06be78f5
Monika <monika.mathur@meta.com>
2020-02-04 18:14:27 +0530

    changes

--- src/app/watch-data.ts
+++ src/app/watch-data.ts
@@ -3,7 +3,7 @@
                  {  
                     "id":           1,
                     "company":         "Reliance Industries",   
-                    "Weightage":     "",
+                    "Weightage":     "",  
                     "isRightStock":     "true",
                     "currentPrice":     "1,421.0(2.54%)",
                     "weekHigh": "1,617.8",
@@ -53,7 +53,404 @@
                     "currentPrice":     "1,008.5(2.23%)",
                     "weekHigh": "1,149.0",
                     "weekLow": "525.1"
-                 }
+                 },
+                 {  
+                  "id":           7,
+                  "company":         "Reliance Industries",   
+                  "Weightage":     "",  
+                  "isRightStock":     "true",
+                  "currentPrice":     "1,421.0(2.54%)",
+                  "weekHigh": "1,617.8",
+                  "weekLow": "1,095.7"
+               },
+               {  
+                  "id":           8,
+                  "company":         "HDFC Bank",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "1,226.0(2.76%)",
+                  "weekHigh": "1,304.1",
+                  "weekLow": "1,035.1"
+               },
+               {  
+                  "id":           9,
+                  "company":         "ICICI Bank",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "532.6(3.30%)",
+                  "weekHigh": "552.4",
+                  "weekLow": "336.3"
+               },
+               {  
+                  "id":           10,
+                  "company":         " ITC",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "214.5(3.27%)",
+                  "weekHigh": "310.0",
+                  "weekLow": "205.0"
+               },
+               {  
+                  "id":           11,
+                  "company":         "HDFC Asset Mngt. Co",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "3,143.4(2.21%)",
+                  "weekHigh": "3,844.0",
+                  "weekLow": "1,302.0"
+               },
+               {  
+                  "id":           12,
+                  "company":         " Polycab India",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "1,008.5(2.23%)",
+                  "weekHigh": "1,149.0",
+                  "weekLow": "525.1"
+               },
+               {  
+                  "id":           13,
+                  "company":         "Reliance Industries",   
+                  "Weightage":     "",  
+                  "isRightStock":     "true",
+                  "currentPrice":     "1,421.0(2.54%)",
+                  "weekHigh": "1,617.8",
+                  "weekLow": "1,095.7"
+               },
+               {  
+                  "id":           14,
+                  "company":         "HDFC Bank",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "1,226.0(2.76%)",
+                  "weekHigh": "1,304.1",
+                  "weekLow": "1,035.1"
+               },
+               {  
+                  "id":           15,
+                  "company":         "ICICI Bank",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "532.6(3.30%)",
+                  "weekHigh": "552.4",
+                  "weekLow": "336.3"
+               },
+               {  
+                  "id":           16,
+                  "company":         " ITC",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "214.5(3.27%)",
+                  "weekHigh": "310.0",
+                  "weekLow": "205.0"
+               },
+               {  
+                  "id":           17,
+                  "company":         "HDFC Asset Mngt. Co",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "3,143.4(2.21%)",
+                  "weekHigh": "3,844.0",
+                  "weekLow": "1,302.0"
+               },
+               {  
+                  "id":           18,
+                  "company":         " Polycab India",   
+                  "Weightage":     "",
+                  "isRightStock":     "true",
+                  "currentPrice":     "1,008.5(2.23%)",
+                  "weekHigh": "1,149.0",
+                  "weekLow": "525.1"
+               },
+               {  
+                "id":               19,
+                "company":         "Reliance Industries",   
+                "Weightage":     "",  
+                "isRightStock":     "true",
+                "currentPrice":     "1,421.0(2.54%)",
+                "weekHigh": "1,617.8",
+                "weekLow": "1,095.7"
+             },
+             {  
+                "id":           20,
+                "company":         "HDFC Bank",   
+                "Weightage":     "",
+                "isRightStock":     "true",
+                "currentPrice":     "1,226.0(2.76%)",
+                "weekHigh": "1,304.1",
+                "weekLow": "1,035.1"
+             },
+             {  
+                "id":           21,
+                "company":         "ICICI Bank",   
+                "Weightage":     "",
+                "isRightStock":     "true",
+                "currentPrice":     "532.6(3.30%)",
+                "weekHigh": "552.4",
+                "weekLow": "336.3"
+             },
+             {  
+                "id":           22,
+                "company":         " ITC",   
+                "Weightage":     "",
+                "isRightStock":     "true",
+                "currentPrice":     "214.5(3.27%)",
+                "weekHigh": "310.0",
+                "weekLow": "205.0"
+             },
+             {  
+                "id":           23,
+                "company":         "HDFC Asset Mngt. Co",   
+                "Weightage":     "",
+                "isRightStock":     "true",
+                "currentPrice":     "3,143.4(2.21%)",
+                "weekHigh": "3,844.0",
+                "weekLow": "1,302.0"
+             },
+             {  
+                "id":           24,
+                "company":         " Polycab India",   
+                "Weightage":     "",
+                "isRightStock":     "true",
+                "currentPrice":     "1,008.5(2.23%)",
+                "weekHigh": "1,149.0",
+                "weekLow": "525.1"
+             },
+             {  
+               "id":           25,
+               "company":         "HDFC Bank",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "1,226.0(2.76%)",
+               "weekHigh": "1,304.1",
+               "weekLow": "1,035.1"
+            },
+            {  
+               "id":           26,
+               "company":         "ICICI Bank",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "532.6(3.30%)",
+               "weekHigh": "552.4",
+               "weekLow": "336.3"
+            },
+            {  
+               "id":           27,
+               "company":         " ITC",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "214.5(3.27%)",
+               "weekHigh": "310.0",
+               "weekLow": "205.0"
+            },
+            {  
+               "id":           28,
+               "company":         "HDFC Asset Mngt. Co",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "3,143.4(2.21%)",
+               "weekHigh": "3,844.0",
+               "weekLow": "1,302.0"
+            },
+            {  
+               "id":           29,
+               "company":         " Polycab India",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "1,008.5(2.23%)",
+               "weekHigh": "1,149.0",
+               "weekLow": "525.1"
+            },
+            {  
+               "id":           30,
+               "company":         " Polycab India",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "1,008.5(2.23%)",
+               "weekHigh": "1,149.0",
+               "weekLow": "525.1"
+            },
+            {  
+               "id":           31,
+               "company":         "HDFC Asset Mngt. Co",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "3,143.4(2.21%)",
+               "weekHigh": "3,844.0",
+               "weekLow": "1,302.0"
+            },
+            {  
+               "id":           32,
+               "company":         " Polycab India",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "1,008.5(2.23%)",
+               "weekHigh": "1,149.0",
+               "weekLow": "525.1"
+            },
+            {  
+               "id":           33,
+               "company":         "Reliance Industries",   
+               "Weightage":     "",  
+               "isRightStock":     "true",
+               "currentPrice":     "1,421.0(2.54%)",
+               "weekHigh": "1,617.8",
+               "weekLow": "1,095.7"
+            },
+            {  
+               "id":           34,
+               "company":         "HDFC Bank",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "1,226.0(2.76%)",
+               "weekHigh": "1,304.1",
+               "weekLow": "1,035.1"
+            },
+            {  
+               "id":           35,
+               "company":         "ICICI Bank",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "532.6(3.30%)",
+               "weekHigh": "552.4",
+               "weekLow": "336.3"
+            },
+            {  
+               "id":           36,
+               "company":         " ITC",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "214.5(3.27%)",
+               "weekHigh": "310.0",
+               "weekLow": "205.0"
+            },
+            {  
+               "id":           37,
+               "company":         "HDFC Asset Mngt. Co",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "3,143.4(2.21%)",
+               "weekHigh": "3,844.0",
+               "weekLow": "1,302.0"
+            },
+            {  
+               "id":           38,
+               "company":         " Polycab India",   
+               "Weightage":     "",
+               "isRightStock":     "true",
+               "currentPrice":     "1,008.5(2.23%)",
+               "weekHigh": "1,149.0",
+               "weekLow": "525.1"
+            },
+            {  
+             "id":               39,
+             "company":         "Reliance Industries",   
+             "Weightage":     "",  
+             "isRightStock":     "true",
+             "currentPrice":     "1,421.0(2.54%)",
+             "weekHigh": "1,617.8",
+             "weekLow": "1,095.7"
+          },
+          {  
+             "id":           40,
+             "company":         "HDFC Bank",   
+             "Weightage":     "",
+             "isRightStock":     "true",
+             "currentPrice":     "1,226.0(2.76%)",
+             "weekHigh": "1,304.1",
+             "weekLow": "1,035.1"
+          },
+          {  
+             "id":           41,
+             "company":         "ICICI Bank",   
+             "Weightage":     "",
+             "isRightStock":     "true",
+             "currentPrice":     "532.6(3.30%)",
+             "weekHigh": "552.4",
+             "weekLow": "336.3"
+          },
+          {  
+             "id":           42,
+             "company":         " ITC",   
+             "Weightage":     "",
+             "isRightStock":     "true",
+             "currentPrice":     "214.5(3.27%)",
+             "weekHigh": "310.0",
+             "weekLow": "205.0"
+          },
+          {  
+             "id":           43,
+             "company":         "HDFC Asset Mngt. Co",   
+             "Weightage":     "",
+             "isRightStock":     "true",
+             "currentPrice":     "3,143.4(2.21%)",
+             "weekHigh": "3,844.0",
+             "weekLow": "1,302.0"
+          },
+          {  
+             "id":           44,
+             "company":         " Polycab India",   
+             "Weightage":     "",
+             "isRightStock":     "true",
+             "currentPrice":     "1,008.5(2.23%)",
+             "weekHigh": "1,149.0",
+             "weekLow": "525.1"
+          },
+          {  
+            "id":           45,
+            "company":         "HDFC Bank",   
+            "Weightage":     "",
+            "isRightStock":     "true",
+            "currentPrice":     "1,226.0(2.76%)",
+            "weekHigh": "1,304.1",
+            "weekLow": "1,035.1"
+         },
+         {  
+            "id":           46,
+            "company":         "ICICI Bank",   
+            "Weightage":     "",
+            "isRightStock":     "true",
+            "currentPrice":     "532.6(3.30%)",
+            "weekHigh": "552.4",
+            "weekLow": "336.3"
+         },
+         {  
+            "id":           47,
+            "company":         " ITC",   
+            "Weightage":     "",
+            "isRightStock":     "true",
+            "currentPrice":     "214.5(3.27%)",
+            "weekHigh": "310.0",
+            "weekLow": "205.0"
+         },
+         {  
+            "id":           48,
+            "company":         "HDFC Asset Mngt. Co",   
+            "Weightage":     "",
+            "isRightStock":     "true",
+            "currentPrice":     "3,143.4(2.21%)",
+            "weekHigh": "3,844.0",
+            "weekLow": "1,302.0"
+         },
+         {  
+            "id":           49,
+            "company":         " Polycab India",   
+            "Weightage":     "",
+            "isRightStock":     "true",
+            "currentPrice":     "1,008.5(2.23%)",
+            "weekHigh": "1,149.0",
+            "weekLow": "525.1"
+         },
+         {  
+            "id":           50,
+            "company":         " Polycab India",   
+            "Weightage":     "",
+            "isRightStock":     "true",
+            "currentPrice":     "1,008.5(2.23%)",
+            "weekHigh": "1,149.0",
+            "weekLow": "525.1"
+         }
+                 
                
     ]  
 }
--- src/app/watch/watch.page.ts
+++ src/app/watch/watch.page.ts
@@ -1,34 +1,63 @@
-import { Component } from '@angular/core';
-import {watchData} from '../watch-data'
+import { Component, ViewChild } from '@angular/core';
+import {watchData} from '../watch-data';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-watch',
   templateUrl: 'watch.page.html',
   styleUrls: ['watch.page.scss'],
 })
 export class WatchPage {
-
+  @ViewChild(IonInfiniteScroll,{ static: true }) infiniteScroll: IonInfiniteScroll;
+  start = 0
+  noOfElement = 10
   private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
-  public items  = watchData.watch;
-  constructor() {
-    // for (let i = 1; i < 11; i++) {
-    //   this.items.push({
-    //     title: 'Item ' + i,
-    //     note: 'This is item #' + i,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
-  }
+  public items  = [];
+  data = watchData.watch;
+  constructor(public toastController: ToastController) {
+    this.items = this.data.slice(this.start,this.noOfElement)
+    this.start = this.noOfElement;
 
+
+  }
+  loadData(event) {
+    setTimeout(() => {
+      this.noOfElement = this.start+ this.noOfElement;
+      console.log('this.start... ', this.start);
+      console.log('this.noOfElement...', this.noOfElement);
+      let x = this.data.slice(this.start,this.noOfElement)
+      // this.items.push(x)
+      for (var i = 0; i < 9; i++) {
+        this.items.push( x[i] );
+      }
+      console.log('this.items.length', this.items);
+
+      console.log('this.items.length', this.items.length);
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.items.length >= 50) {
+        event.target.disabled = true;
+        this.infiniteScroll.disabled  = true;
+      }
+    }, 500);
+  }
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
+  deleteItem(index){
+    this.items.splice(index, 1);
+  }
+  async presentToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000
+    }); 
+    toast.present();
+  }
+  onClickAlert(){
+   this.presentToast("Alert has set successfully")
+  }
 }
