2e3b123fe2dd2e90d556cc8b09a5f2d55c7a4274
Monika <monika.mathur@meta.com>
2020-02-04 18:19:31 +0530

    json

--- src/app/watch/watch.page.ts
+++ src/app/watch/watch.page.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import {watchData} from '../watch-data';
+import {watchData} from '../\watch-data';
 import { IonInfiniteScroll } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
 
@@ -14,42 +14,36 @@ export class WatchPage {
   noOfElement = 10
   private selectedItem: any;
   public items  = [];
-  data = watchData.watch;
+  watchList = watchData.watch;
   constructor(public toastController: ToastController) {
-    this.items = this.data.slice(this.start,this.noOfElement)
+    this.items = this.watchList.slice(this.start,this.noOfElement)
     this.start = this.noOfElement;
-
-
   }
+
   loadData(event) {
     setTimeout(() => {
       this.noOfElement = this.start+ this.noOfElement;
-      console.log('this.start... ', this.start);
-      console.log('this.noOfElement...', this.noOfElement);
-      let x = this.data.slice(this.start,this.noOfElement)
-      // this.items.push(x)
+      let slicedList = this.watchList.slice(this.start,this.noOfElement)
       for (var i = 0; i < 9; i++) {
-        this.items.push( x[i] );
+        this.items.push( slicedList[i] );
       }
-      console.log('this.items.length', this.items);
-
-      console.log('this.items.length', this.items.length);
       event.target.complete();
 
-      // App logic to determine if all data is loaded
-      // and disable the infinite scroll
       if (this.items.length >= 50) {
         event.target.disabled = true;
         this.infiniteScroll.disabled  = true;
       }
     }, 500);
   }
+
   toggleInfiniteScroll() {
     this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
+
   deleteItem(index){
     this.items.splice(index, 1);
   }
+
   async presentToast(message) {
     const toast = await this.toastController.create({
       message: message,
@@ -57,6 +51,7 @@ export class WatchPage {
     }); 
     toast.present();
   }
+
   onClickAlert(){
    this.presentToast("Alert has set successfully")
   }
